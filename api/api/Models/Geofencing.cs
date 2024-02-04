141b20759ba3ae37e4fb2903a853497259d74bc3
André Silva <andre06x@gmail.com>
2024-02-04 20:09:27 -0300

    add model geofencing

---
+++ api/api/Models/Geofencing.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+
+    public class Geofencing
+    {
+        [Column("id")]
+        public Guid id { get; set; }
+
+        [Column("latitude")]
+        public double latitude { get; set; }
+
+        [Column("longitude")]
+        public double longitude { get; set; }
+
+        [Column("horario")]
+        public DateTime horario { get; set; }
+
+        [Column("tipo")]
+        public string tipo { get; set; }
+    }
+}
