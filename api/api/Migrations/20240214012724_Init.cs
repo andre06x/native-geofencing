20cb9e2320b6f5630f6469d1b8ab4a240384db0b
André Silva <andre06x@gmail.com>
2024-02-13 22:39:22 -0300

    add migration

---
+++ api/api/Migrations/20240214012724_Init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace geofencing.api.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Geofencing",
+                columns: table => new
+                {
+                    horario = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    latitude = table.Column<double>(type: "double precision", nullable: false),
+                    longitude = table.Column<double>(type: "double precision", nullable: false),
+                    tipo = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Geofencing", x => x.horario);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Geofencing");
+        }
+    }
+}
