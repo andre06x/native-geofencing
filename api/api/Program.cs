e83895ec9ece7a919912de09f0842c3223c5b4b1
André Silva <andre06x@gmail.com>
2024-02-13 22:39:42 -0300

    add connection string

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -1,3 +1,6 @@
+using api.Configuracao;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+string databaseUrl = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT_CONNECTSTRING");
+
+if (databaseUrl == null)
+{
+    databaseUrl = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+
+
+builder.Services.AddDbContext<Contexto>(options =>
+    options.UseNpgsql(databaseUrl, npgsqlOptions =>
+        npgsqlOptions.MigrationsAssembly(typeof(Program).Assembly.FullName)
+    )
+);
 
 var app = builder.Build();
 
