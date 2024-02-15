168bf1bf8be3e6ed46c70428f14417dabc34648d
André Silva <andre06x@gmail.com>
2024-02-14 22:19:05 -0300

    add get post controller

---
+++ api/api/Controllers/GeofencingController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using api.Models;
+using api.Configuracao;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GeofencingController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public GeofencingController(Contexto context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Geofencing>>> GetGeofencings()
+        {
+            var geofencings = await _context.Geofencing.ToListAsync();
+
+            if (geofencings == null || geofencings.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return geofencings;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Geofencing>> PostGeofencing(Geofencing geofencing)
+        {
+            if (_context.Geofencing == null)
+            {
+                return Problem("Entity set 'Contexto.Geofencing'  is null.");
+            }
+            _context.Geofencing.Add(geofencing);
+            await _context.SaveChangesAsync();
+
+            return geofencing;
+        }
+    }
+}
